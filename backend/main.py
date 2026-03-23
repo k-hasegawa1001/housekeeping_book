330f3bf9bc8eec553ebf252e3b7294d81bc46366
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2026-03-23 11:22:33 +0900

    create: init FastAPI app

--- backend/main.py
+++ backend/main.py
@@ -4,4 +4,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "FastAPI is running!"}
+    return {"message": "FastAPI is running behind Nginx!"}
+
+@app.get("/test")
+def test_endpoint():
+    return {"message": "This is a test endpoint."}
