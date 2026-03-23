3af9f69c4dea0597756850711320db74b2e4c08c
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2026-03-23 14:03:48 +0900

    change: Allow access from domains other than localhost.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // 外部からのアクセスを許可するホスト名を追加
+    allowedHosts: ["docker-ec2-ohara.duckdns.org"],
+  },
+});
